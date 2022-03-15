3bc01799be68f05358533b25d9d0ac9ed3c84db3
AzmammadCabrayilov <7c0dj00@code.edu.az>
2022-03-15 08:46:14 +0400

    Hometask

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 Sade ededdirmi?
+            #region   
+            //int halfNumber = 0;
+            //bool isTrue;
+            //Console.WriteLine("Ededi daxil edin");                                //alqoritmi qurmusam ama neyise duz yazmiram
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //halfNumber = number / 2;
+
+            //for (int i = 2; i <= halfNumber; i++)
+            //{
+            //    if (halfNumber % i == 0)
+            //    {
+            //        Console.WriteLine("Eded sade deyil");
+            //        isTrue = true;
+            //        break;
+            //    }
+
+            //}
+            //if (isTrue = false)
+            //{
+            //    Console.WriteLine("Eded sadedir");
+            //}
+            #endregion
+
+
+            //Task 2 Nece mertebeli oldugu
+            #region
+            //Console.WriteLine("Ededi daxil edin");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int result = 1;
+
+            //if (number < 0)
+            //{
+            //    number = number * (-1);
+            //}
+            //while (number > 9)
+            //{
+            //    number /= 10;
+            //    result++;
+            //}
+            //Console.WriteLine(result);
+            #endregion
+
+
+            //Task3 polindrom
+            #region
+            //int sum = 0, r;
+            //Console.WriteLine("ededi daxil edin");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //int copyNumber = number;
+
+            //while (number > 0)
+            //{
+            //    r = number % 10;
+            //    sum = (sum * 10) + r;
+            //    number = number / 10;
+            //}
+            //if (copyNumber == sum)
+            //{
+            //    Console.WriteLine("Palindromdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Palindrom deyil");
+            //}
+            #endregion
+
+        }
+    }
+}
