8355afcb2d48c37b400c88ae184f63782b7863f3
AzmammadCabrayilov <100667499+AzmammadCabrayilov@users.noreply.github.com>
2022-03-29 01:03:48 +0400

    28.03.2022 Home Task
    28.03.2022 Home Task

---
+++ 28.03.2022 HomeTask/ConsoleApp2/ConsoleApp2/Models/Singer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    class Singer
+    {
+        private string _Name;
+        private string _SurName;
+        public int _Age;
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 100)
+                {
+
+                    _Name = value;
+                }
+            }
+        }
+
+        public string SurName
+        {
+            get
+            {
+                return _SurName;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 100)
+                {
+                    _SurName = value;
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                if (value > 0 && value <= 170)
+                {
+                    _Age = value;
+                }
+            }
+        }
+
+        public Singer(string name,string surname, int age)
+        {
+            Name = name;
+            SurName = surname;
+            Age = age;
+        }
+    }
+}
---
+++ 28.03.2022 HomeTask/ConsoleApp2/ConsoleApp2/Models/Song.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    class Song
+    {
+        public Song()
+        {
+
+        }
+
+        private string _SongName;
+        private string _Genre;
+        private Singer _Singer;
+        private int[] Ratings = new int[0];
+
+
+        public string SongName
+        {
+            get
+            {
+                return _SongName;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length < 100)
+                {
+
+                    _SongName = value;
+                }
+            }
+        }
+
+        public string Genre
+        {
+            get
+            {
+                return _Genre;
+            }
+            set
+            {
+                string[] genreProperties = { "Pop", "Rock", "Jazz", "Techno" };
+                foreach (var item in genreProperties)
+                {
+                    if (item == value)
+                    {
+                        _Genre = value;
+                    }
+                }
+                
+            }
+        }
+
+        public Singer Singer
+        {
+            get
+            {
+                return _Singer;
+            }
+            set
+            {
+                _Singer = value;
+            }
+        }
+
+
+        public Song(string songName, string genre , Singer singer)
+        {
+            SongName = songName;
+            Genre = genre;
+            Singer = singer;
+        }
+
+        public void AddRating(int rating)
+        {
+            if (rating >= 0 && rating <= 5)
+            {
+                Array.Resize(ref Ratings, Ratings.Length + 1);
+                Ratings[Ratings.Length - 1] = rating;
+            }
+        }
+
+        public double GetAverageRating()
+        {
+            return Math.Round(Ratings.Average(), 2);
+        }
+
+    }
+}
+
